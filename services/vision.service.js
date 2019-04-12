6db51cf08d868cddb9f95d1f8c437a4b12110357
Alfredo-Git <algonzalezramirez@gmail.com>
2019-04-12 19:21:16 +0200

    css correctly finished

--- services/vision.service.js
+++ services/vision.service.js
@@ -5,7 +5,7 @@ const vision = require('@google-cloud/vision');
 const client = new vision.ImageAnnotatorClient();
 
 module.exports.getLabel = (photo) => {  
-    return axios.post('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBD_cOR_JAxWx5tbcaYK-9IFUS4-GVjv8E', {
+    return axios.post(GOOGLE_AXIOS, {
         requests: [
             {
                 image: {
